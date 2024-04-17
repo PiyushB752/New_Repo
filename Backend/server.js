67469da6c008e9baae5793aea02047c8bdbb9472
Piyush Bachani <Piyush>
2024-04-17 16:19:25 +0530

    Done

--- Backend/server.js
+++ Backend/server.js
@@ -13,10 +13,38 @@ app.get("/",async(req,res)=>{
 
 app.post("/post",async(req,res)=>{
     let body = req.body
+
+    let data = await User.find({email:body.email})
+    if(data.length > 0){
+        return res.status(401).send({message:"user already exist."})
+    }
+
     try{
         let value = await User.insertMany(body)
         res.status(201).send({message:"Data created",value})
     }catch(error){
+        console.log(error)
+    }
+})
+
+app.patch("/update/:id",async (req,res)=>{
+    let body = req.body
+    let id = req.params.id
+    
+    try {
+        let value = await User.findByIdAndUpdate(id,body,{ new: true })
+        res.status(201).send({message:"data uploaded",value})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.delete("/delete/:id",async(req,res)=>{
+    let id = req.params.id
+    try {
+        let del = await User.findByIdAndDelete(id)
+        res.status(200).send({message:"Deleted sucessfully"})
+    } catch (error) {
         console.log(error)
     }
 })
